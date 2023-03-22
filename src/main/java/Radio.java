3e7c77171cfdf91db8699301643a03837dbd02fa
romanmihailow <romanmihailow@email.com>
2023-03-22 16:12:05 +0500

    constructor done

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,13 +1,45 @@
+import com.sun.nio.sctp.Association;
+
 public class Radio {
 
-    private int currentVolume;
+    private int currentVolume=5;
     private int currentChannel;
-    //private int minNumberChannel = 0;
-    //private int countChannel = 10;
+    private int minNumberChannel = 0;
+    private int countChannel = 10;
+    private int maxNumberChannel = countChannel - 1;
 
-    //public Radio() {
+    public Radio(int countChannel, int setCurrentVolume) {
 
-    //}
+        if (setCurrentVolume < 0 ) {
+            return;
+        }
+        if ( setCurrentVolume > countChannel - 1 ) {
+            return;
+        }
+        currentVolume = setCurrentVolume;
+    }
+
+//    public Radio (int setIncreaseVolume) {
+//        if (setIncreaseVolume < 100) {
+//            currentVolume = currentVolume + 1;
+//        } else {
+//            currentVolume = 100;
+//        }
+//
+//    }
+
+    public Radio(int setIncreaseVolume) {
+        if (setIncreaseVolume < 100) {
+            currentVolume = setIncreaseVolume + 1;
+        } else {
+            currentVolume = 100;
+        }
+    }
+
+
+    public int getCountChannel() {
+        return countChannel;
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
@@ -17,17 +49,17 @@ public class Radio {
         return currentChannel;
     }
 
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < 0 ) {
-            return;
-        }
-        if (newCurrentVolume > 100 ) {
-            return;
-        }
-
-        currentVolume = newCurrentVolume;
-
-    }
+//    public void setCurrentVolume(int newCurrentVolume) {
+//        if (newCurrentVolume < 0 ) {
+//            return;
+//        }
+//        if (newCurrentVolume > 100 ) {
+//            return;
+//        }
+//
+//        currentVolume = newCurrentVolume;
+//
+//    }
     public void setCurrentChannel(int newCurrentChannel) {
         if (newCurrentChannel < 0 ) {
             return;
@@ -42,14 +74,14 @@ public class Radio {
 
 
 
-    public void setIncreaseVolume() {
-
-        if (currentVolume < 100) {
-            currentVolume = currentVolume + 1;
-        } else {
-            currentVolume = 100;
-        }
-    }
+//    public void setIncreaseVolume() {
+//
+//        if (currentVolume < 100) {
+//            currentVolume = currentVolume + 1;
+//        } else {
+//            currentVolume = 100;
+//        }
+//    }
 
 
 
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,3 +1,4 @@
+import com.sun.nio.sctp.Association;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -5,106 +6,125 @@ public class RadioTest {
     //flexible on
 
     @Test
-    public void shouldIncreaseVolume() {
-        Radio radioVolume = new Radio();
-        radioVolume.setCurrentVolume(0);
-        radioVolume.setIncreaseVolume();
-        int expected = 1;
-        int actual = radioVolume.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);          //   увеличение звука на 1 работает
-    }
-
-    @Test
-    public void shouldIncreaseVolume100() {
-        Radio radioVolume = new Radio();
-        radioVolume.setCurrentVolume(100);
-        radioVolume.setIncreaseVolume();
-        int expected = 100;
-        int actual = radioVolume.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);      //   больше максимальной громкости 100 звук прибавить нельзя
-    }
-    @Test
-    public void shouldDecreaseVolume0() {
-        Radio radioVolume = new Radio();
-        radioVolume.setCurrentVolume(0);
-        radioVolume.setDecreaseVolume();
-        int expected = 0;
-        int actual = radioVolume.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);       // ниже 0 звук убавить нельзя))))))))
-    }
-
-    @Test
-    public void shouldDecreaseVolume() {
-        Radio radioVolume = new Radio();
-        radioVolume.setCurrentVolume(100);
-        radioVolume.setDecreaseVolume();
-        int expected = 99;
-        int actual = radioVolume.getCurrentVolume();
-        Assertions.assertEquals(expected, actual);    //   убавить звук на 1 работает
-    }
+    public void test() {
+        Radio radioChannel = new Radio(10, 9);
+        Assertions.assertEquals(10, radioChannel.getCountChannel());  //проверка количества каналов
+        Assertions.assertEquals(10, radioChannel.getCurrentVolume());  //проверка установки максимального канала
 
 
-
-    @Test
-    public void shouldSwitchNextChannel0() {
-        Radio radioChannel = new Radio();
-        radioChannel.next();
-        int expected = 1;
-        int actual = radioChannel.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void shouldSwitchNextChannel() {
-        Radio radioChannel = new Radio();
-        radioChannel.setCurrentChannel(9);
-        radioChannel.next();
-        int expected = 0;
-        int actual = radioChannel.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void shouldSwitchPrevChannel9() {
-        Radio radioChannel = new Radio();
-        radioChannel.setCurrentChannel(9);
-        radioChannel.prev();
-        int expected = 8;
-        int actual = radioChannel.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
 
     }
     @Test
-    public void shouldSwitchPrevChannel() {
-        Radio radioChannel = new Radio();
-        radioChannel.prev();
-        int expected = 9;
-        int actual = radioChannel.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
-
+    public void testw(){
+        Radio radioVolume = new Radio (90);
+        Assertions.assertEquals(91, radioVolume.getCurrentVolume());
     }
-    @Test
-    public void shouldSwitchChannelMore9() {
-        Radio radioChannel = new Radio();
-        radioChannel.setCurrentChannel(10);
-        radioChannel.prev();
-        int expected = 9;
-        int actual = radioChannel.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
 
-    }
-    @Test
-    public void shouldSwitchChannelLessThan0() {
-        Radio radioChannel = new Radio();
-        radioChannel.setCurrentChannel(-1);
-        radioChannel.prev();
-        int expected = 9;
-        int actual = radioChannel.getCurrentChannel();
-        Assertions.assertEquals(expected, actual);
 
-    }
+
+
+
+//    @Test
+//    public void shouldIncreaseVolume() {
+//        Radio radioVolume = new Radio();
+//        radioVolume.setCurrentVolume(0);
+//        radioVolume.setIncreaseVolume();
+//        int expected = 1;
+//        int actual = radioVolume.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);          //   увеличение звука на 1 работает
+//    }
+//
+//    @Test
+//    public void shouldIncreaseVolume100() {
+//        Radio radioVolume = new Radio();
+//        radioVolume.setCurrentVolume(100);
+//        radioVolume.setIncreaseVolume();
+//        int expected = 100;
+//        int actual = radioVolume.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);      //   больше максимальной громкости 100 звук прибавить нельзя
+//    }
+//    @Test
+//    public void shouldDecreaseVolume0() {
+//        Radio radioVolume = new Radio();
+//        radioVolume.setCurrentVolume(0);
+//        radioVolume.setDecreaseVolume();
+//        int expected = 0;
+//        int actual = radioVolume.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);       // ниже 0 звук убавить нельзя))))))))
+//    }
+//
+//    @Test
+//    public void shouldDecreaseVolume() {
+//        Radio radioVolume = new Radio();
+//        radioVolume.setCurrentVolume(100);
+//        radioVolume.setDecreaseVolume();
+//        int expected = 99;
+//        int actual = radioVolume.getCurrentVolume();
+//        Assertions.assertEquals(expected, actual);    //   убавить звук на 1 работает
+//    }
+//
+//
+//
+//    @Test
+//    public void shouldSwitchNextChannel0() {
+//        Radio radioChannel = new Radio();
+//        radioChannel.next();
+//        int expected = 1;
+//        int actual = radioChannel.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
+//
+//    @Test
+//    public void shouldSwitchNextChannel() {
+//        Radio radioChannel = new Radio();
+//        radioChannel.setCurrentChannel(9);
+//        radioChannel.next();
+//        int expected = 0;
+//        int actual = radioChannel.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
+//
+//    @Test
+//    public void shouldSwitchPrevChannel9() {
+//        Radio radioChannel = new Radio();
+//        radioChannel.setCurrentChannel(9);
+//        radioChannel.prev();
+//        int expected = 8;
+//        int actual = radioChannel.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
+//    @Test
+//    public void shouldSwitchPrevChannel() {
+//        Radio radioChannel = new Radio();
+//        radioChannel.prev();
+//        int expected = 9;
+//        int actual = radioChannel.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
+//    @Test
+//    public void shouldSwitchChannelMore9() {
+//        Radio radioChannel = new Radio();
+//        radioChannel.setCurrentChannel(10);
+//        radioChannel.prev();
+//        int expected = 9;
+//        int actual = radioChannel.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
+//    @Test
+//    public void shouldSwitchChannelLessThan0() {
+//        Radio radioChannel = new Radio();
+//        radioChannel.setCurrentChannel(-1);
+//        radioChannel.prev();
+//        int expected = 9;
+//        int actual = radioChannel.getCurrentChannel();
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
 
 
 
